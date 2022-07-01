16f61cf3554570c1497bc0ac069ad22814a68588
daniela ventura <danielavensec@gmail.com>
2022-06-30 20:10:35 -0700

    modify

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ console.log('Hello World!');
 */
 console.log("Daniela");
 console.log("sopes");
+
+//Day 2
+    //Strings 
+        const personName="Daniela Ventura"; //Double quote
+        const title = 'student';// Single quote
+        const nameTag1= 
+        
+
+    //Daily Challenge
+        let  = `supeercalifragilisticexpialidocious`;
+            console.log('word.length');
+        console.log (A);
+        console.log(3n)
+        console.log(typeof 'word')
